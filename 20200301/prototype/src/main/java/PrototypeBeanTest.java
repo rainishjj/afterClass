fc62afa87ce13dfb6421260064bab10af8ffc0fd
rainishjj <344525925@qq.com>
2020-05-26 10:29:58 +0800

    serlvet

--- 20200301/prototype/src/main/java/PrototypeBeanTest.java
+++ 20200301/prototype/src/main/java/PrototypeBeanTest.java
@@ -1,20 +1,32 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PrototypeBeanTest {
     public static void main(String[] args) {
-        PrototypeBean prototypeBean = new PrototypeBean();
-        prototypeBean.setCount(0);
-        prototypeBean.setName("test");
+        ArrayList a = new ArrayList();
+        a.add(1);
+        a.add(1);
+        System.out.println(a.toString());
 
-        List<String> hobbies = new ArrayList<>();
-        hobbies.add("panio");
-        hobbies.add("other");
-        prototypeBean.setHobbies(hobbies);
 
-        PrototypeBean cloneBean = CloneUtil.clone(prototypeBean);
+        LinkedList b = new LinkedList();
+        b.add(1);
+        b.add(1);
+        System.out.println(b.toString());
 
-        System.out.println(prototypeBean == cloneBean);
-        System.out.println(prototypeBean.getHobbies() == cloneBean.getHobbies());
+//        PrototypeBean prototypeBean = new PrototypeBean();
+//        prototypeBean.setCount(0);
+//        prototypeBean.setName("test");
+//
+//        List<String> hobbies = new ArrayList<>();
+//        hobbies.add("panio");
+//        hobbies.add("other");
+//        prototypeBean.setHobbies(hobbies);
+//
+//        PrototypeBean cloneBean = CloneUtil.clone(prototypeBean);
+//
+//        System.out.println(prototypeBean == cloneBean);
+//        System.out.println(prototypeBean.getHobbies() == cloneBean.getHobbies());
     }
 }
