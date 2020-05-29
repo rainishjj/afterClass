b9a9db1cab06c1484df03c34b9b429b535179d52
rainishjj <344525925@qq.com>
2020-05-29 15:07:15 +0800

    自己实现阻塞队列-后进先出

--- 20200301/prototype/src/main/java/com/rainish/juc/RainishQueue.java
+++ 20200301/prototype/src/main/java/com/rainish/juc/RainishQueue.java
@@ -6,6 +6,13 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 利用arrayList来充当队列的数据结构。即是线性结构，不是链表。其中会有扩容的问题，即arrayList是个无限数组，不会存在满的情况。
+ * 要实现阻塞队列，则需要控制其大小，不允许进行扩容。所以在判断size的时候，需要加上volatile关键字，让其可见
+ *
+ * 阻塞队列的特性：队列空了，取数据的线程等待；队列满了，放元素的线程等待
+ *
+ */
 public class RainishQueue {
     private int maxSize = 2;
     private volatile List list = new ArrayList(maxSize);
